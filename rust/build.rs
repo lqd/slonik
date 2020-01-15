f4783a94b6d75e6c660d691c21549ad888c4619f
lqd <remy.rakic+github@gmail.com>
2020-01-15 14:56:15 +0100

    build: handle errors in binding generation for IDE support
    `cbindgen::generate_with_config` can fail to generate bindings in an IDE environment: by handle the error gracefully, RLS will be able to continue its analysis

--- rust/build.rs
+++ rust/build.rs
@@ -6,7 +6,7 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut config: cbindgen::Config = Default::default();
     config.language = cbindgen::Language::C;
-    cbindgen::generate_with_config(&crate_dir, config)
-      .unwrap()
-      .write_to_file("target/postgres_ffi.h");
+    if let Ok(bindings) = cbindgen::generate_with_config(&crate_dir, config) {
+        bindings.write_to_file("target/postgres_ffi.h");
+    }
 }
